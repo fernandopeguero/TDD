18237574546ae06012ffd465efdad997a0d4b807
Fernando Peguero <fernandopeguerofs@gmail.com>
2025-04-06 23:14:04 -0400

    Add babel config to webpack module

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,38 @@ module.exports = {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
     },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: ["node_modules"],
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.gif$/,
+                type: "asset/inline",
+            },
+            {
+                test: /\.(ttf|eot|svg)$/,
+                type: "asset/resource",
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            config$: "./configs/app-config.js",
+            react: "./vendor/react-master",
+        },
+        extensions: [".js", ".jsx"],
+        modules: [
+            "node_modules",
+            "bower_components",
+            "shared",
+            "/shared/vendor/modules",
+        ],
+    },
 };
