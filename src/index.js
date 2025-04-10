e9a42195730eab0f9b614e8c250dedfe07779a4f
Fernando Peguero <fernandopeguerofs@gmail.com>
2025-04-09 23:46:50 -0400

    Add alpha numeric logic

--- src/index.js
+++ src/index.js
@@ -79,6 +79,25 @@ function caesarCypher(letters, shiftIndex) {
     return result;
 }
 
+// alpha numeric
+
+function isAlphaNumeric(str) {
+    var code, i, len;
+
+    for (i = 0, len = str.length; i < len; i++) {
+        code = str.charCodeAt(i);
+        if (
+            !(code > 47 && code < 58) && // numeric (0-9)
+            !(code > 64 && code < 91) && // upper alpha (A-Z)
+            !(code > 96 && code < 123)
+        ) {
+            // lower alpha (a-z)
+            return false;
+        }
+    }
+    return true;
+}
+
 test("Shift cypher by 3", () => {
     const result = caesarCypher("xyz", 3);
 
